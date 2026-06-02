b8032aa114c3b185f3278cd204a15534e4fe60b9
SUSH9391 <masrbaliga@gmail.com>
2026-06-02 12:34:34 +0530

    tfs tested with adding curl commands to setup.sh

--- course4/week3-ungraded-labs/C4_W3_Lab_3_TFS_Model_Versioning/setup.sh
+++ course4/week3-ungraded-labs/C4_W3_Lab_3_TFS_Model_Versioning/setup.sh
@@ -1,11 +1,38 @@
-wget -q -P ./models/animals/1/ https://storage.googleapis.com/mlep-public/course_1/week2/model-balanced/saved_model.pb
-wget -q -P ./models/animals/1/variables/ https://storage.googleapis.com/mlep-public/course_1/week2/model-balanced/variables/variables.data-00000-of-00001
-wget -q -P ./models/animals/1/variables/ https://storage.googleapis.com/mlep-public/course_1/week2/model-balanced/variables/variables.index
+#!/bin/bash
 
-wget -q -P ./models/animals/2/ https://storage.googleapis.com/mlep-public/course_1/week2/model-imbalanced/saved_model.pb
-wget -q -P ./models/animals/2/variables/ https://storage.googleapis.com/mlep-public/course_1/week2/model-imbalanced/variables/variables.data-00000-of-00001
-wget -q -P ./models/animals/2/variables/ https://storage.googleapis.com/mlep-public/course_1/week2/model-imbalanced/variables/variables.index
+# Create directories
+mkdir -p models/animals/1/variables
+mkdir -p models/animals/2/variables
+mkdir -p models/animals/3/variables
 
-wget -q -P ./models/animals/3/ https://storage.googleapis.com/mlep-public/course_1/week2/model-augmented/saved_model.pb
-wget -q -P ./models/animals/3/variables/ https://storage.googleapis.com/mlep-public/course_1/week2/model-augmented/variables/variables.data-00000-of-00001
-wget -q -P ./models/animals/3/variables/ https://storage.googleapis.com/mlep-public/course_1/week2/model-augmented/variables/variables.index
+# Model 1 - Balanced
+curl -L https://storage.googleapis.com/mlep-public/course_1/week2/model-balanced/saved_model.pb \
+    -o models/animals/1/saved_model.pb
+
+curl -L https://storage.googleapis.com/mlep-public/course_1/week2/model-balanced/variables/variables.data-00000-of-00001 \
+    -o models/animals/1/variables/variables.data-00000-of-00001
+
+curl -L https://storage.googleapis.com/mlep-public/course_1/week2/model-balanced/variables/variables.index \
+    -o models/animals/1/variables/variables.index
+
+# Model 2 - Imbalanced
+curl -L https://storage.googleapis.com/mlep-public/course_1/week2/model-imbalanced/saved_model.pb \
+    -o models/animals/2/saved_model.pb
+
+curl -L https://storage.googleapis.com/mlep-public/course_1/week2/model-imbalanced/variables/variables.data-00000-of-00001 \
+    -o models/animals/2/variables/variables.data-00000-of-00001
+
+curl -L https://storage.googleapis.com/mlep-public/course_1/week2/model-imbalanced/variables/variables.index \
+    -o models/animals/2/variables/variables.index
+
+# Model 3 - Augmented
+curl -L https://storage.googleapis.com/mlep-public/course_1/week2/model-augmented/saved_model.pb \
+    -o models/animals/3/saved_model.pb
+
+curl -L https://storage.googleapis.com/mlep-public/course_1/week2/model-augmented/variables/variables.data-00000-of-00001 \
+    -o models/animals/3/variables/variables.data-00000-of-00001
+
+curl -L https://storage.googleapis.com/mlep-public/course_1/week2/model-augmented/variables/variables.index \
+    -o models/animals/3/variables/variables.index
+
+echo "All models downloaded successfully!"
